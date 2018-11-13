0bf9fd533fed17b7c01784cc5790c945361529c4
bidouche <alexetiennelev@gmail.com>
2018-11-13 21:37:04 +0100

    last

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_page#home'
+  get "/profile", to: 'static_page#profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
